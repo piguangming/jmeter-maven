c983a820d47322021699235d872bb975254a9333
zhangtao <zytwell321@163.com>
2017-10-15 18:04:25 +0800

    new

---
+++ src/main/java/jmeter_infTest/jmeter/App.java
@@ -0,0 +1,13 @@
+package jmeter_infTest.jmeter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
